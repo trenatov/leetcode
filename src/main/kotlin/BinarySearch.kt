315618944b0d0c77a6dda6f6ef8e851d136585be
trenatov <temirlan.renatov@gmail.com>
2022-10-05 13:37:47 +0600

    binary search

---
+++ src/main/kotlin/BinarySearch.kt
@@ -0,0 +1,20 @@
+/**
+ * https://leetcode.com/problems/binary-search/?envType=study-plan&id=algorithm-i
+ */
+class BinarySearch : Problem {
+
+    fun search(nums: IntArray, target: Int, mid: Int = nums.size): Int {
+        val currentMid = mid / 2
+
+
+
+        return -1
+    }
+
+    override fun run() {
+        val array = intArrayOf(-1, 0, 3, 5, 9, 12)
+
+        println(search(array, 9))
+    }
+
+}
---
+++ src/main/kotlin/Problem.kt
@@ -0,0 +1,5 @@
+interface Problem {
+
+    fun run()
+
+}
