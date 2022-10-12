4ddcaf0a5d7edfe66d7d657dd911764f469b6ab0
trenatov <temirlan.renatov@gmail.com>
2022-10-12 22:24:46 +0600

    Middle Of THe Linked list

---
+++ src/main/kotlin/MiddleOfTheLinkedList.kt
@@ -0,0 +1,45 @@
+/**
+ * https://leetcode.com/problems/middle-of-the-linked-list/
+ */
+class MiddleOfTheLinkedList : Problem {
+
+    fun middleNode(head: ListNode?): ListNode? {
+        val nodeCount = countNodes(head, 0)
+
+        val middle = nodeCount / 2
+
+        return getMiddleNode(head, middle)
+    }
+
+    private fun getMiddleNode(node: ListNode?, index: Int): ListNode? {
+        if (node == null || index == 0) return node
+
+        return getMiddleNode(node.next, index - 1)
+    }
+
+    private fun countNodes(node: ListNode?, count: Int): Int {
+        if (node == null) return count
+        return countNodes(node.next, count + 1)
+    }
+
+    class ListNode(var `val`: Int) {
+        var next: ListNode? = null
+    }
+
+    override fun run() {
+        val root = ListNode(1).apply {
+            next = ListNode(2).apply {
+                next = ListNode(3).apply {
+                    next = ListNode(4).apply {
+                        next = ListNode(5).apply {
+                            next = ListNode(6)
+                        }
+
+                    }
+                }
+
+            }
+        }
+        println(middleNode(root)?.`val`)
+    }
+}
