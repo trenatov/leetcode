9c2b1f3b5c1164fb1e90b3c1a6bac51092c62744
trenatov <temirlan.renatov@gmail.com>
2022-10-14 18:37:39 +0600

    Buy Sell stocks

---
+++ src/main/kotlin/BuySellStocks.kt
@@ -0,0 +1,31 @@
+/**
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+ */
+class BuySellStocks : Problem {
+
+    fun maxProfit(prices: IntArray): Int {
+        var max = 0
+        var min = prices[0]
+
+        for (i in 1 until prices.size) {
+            if (prices[i] < min) {
+                min = prices[i]
+            } else if (prices[i] - min > max) {
+                max = prices[i] - min
+            }
+        }
+        return max
+    }
+
+    override fun run() {
+        val array = intArrayOf(7, 1, 5, 3, 6, 4)
+        val array2 = intArrayOf(7, 6, 4, 3, 1)
+        val array3 = intArrayOf(2, 4, 1)
+        val array4 = intArrayOf(2, 1, 2, 0, 1)
+        println(maxProfit(array))
+        println(maxProfit(array2))
+        println(maxProfit(array3))
+        println(maxProfit(array4))
+    }
+
+}
