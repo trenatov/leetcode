897b90fb77772cb15ef6d174afeff38506609323
trenatov <temirlan.renatov@gmail.com>
2022-10-15 17:24:06 +0600

    Symmetric tree

---
+++ src/main/kotlin/SymmetricTree.kt
@@ -0,0 +1,34 @@
+/**
+ * https://leetcode.com/problems/symmetric-tree/
+ */
+class SymmetricTree : Problem {
+
+    fun isSymmetric(root: TreeNode?): Boolean {
+        return isSymmetric(root?.left, root?.right)
+    }
+
+    fun isSymmetric(left: TreeNode?, right: TreeNode?): Boolean {
+        if (left == null && right == null) return true
+
+        if (left?.`val` != right?.`val`) return false
+
+        val isLeftSymmetric = isSymmetric(left?.right, right?.left)
+        val isRightSymmetric = isSymmetric(left?.left, right?.right)
+
+        return if (isLeftSymmetric && isRightSymmetric) return true else false
+    }
+
+    class TreeNode(var `val`: Int) {
+        var left: TreeNode? = null
+        var right: TreeNode? = null
+    }
+
+    override fun run() {
+        val root = TreeNode(1).apply {
+            left = TreeNode(2)
+            right = TreeNode(3)
+        }
+
+        println(isSymmetric(root))
+    }
+}
