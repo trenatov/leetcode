b292d6230c6ff4dfd7097ff2405802431b538adc
trenatov <temirlan.renatov@gmail.com>
2022-10-30 22:36:31 +0600

    First unique character

---
+++ src/main/kotlin/FirstUniqueCharacter.kt
@@ -0,0 +1,26 @@
+/**
+ * https://leetcode.com/problems/first-unique-character-in-a-string/
+ */
+class FirstUniqueCharacter : Problem {
+
+    fun firstUniqChar(s: String): Int {
+        val array = s.toCharArray()
+        for (index in array.indices) {
+            if (isUnique(array, index)) return index
+        }
+        return -1
+    }
+
+    private fun isUnique(array: CharArray, index: Int): Boolean {
+        for (i in array.indices) {
+            if (i != index && array[index] == array[i]) return false
+        }
+
+        return true
+    }
+
+    override fun run() {
+        println(firstUniqChar("aabb"))
+    }
+
+}
