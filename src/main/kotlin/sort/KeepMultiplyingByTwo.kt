e4c6b1a01aff2f4d1cbc01a1f065fac8ce897454
trenatov <temirlan.renatov@gmail.com>
2023-01-24 14:24:30 +0600

    Keep multiplying

---
+++ src/main/kotlin/sort/KeepMultiplyingByTwo.kt
@@ -0,0 +1,40 @@
+package sort
+
+import Problem
+
+
+/**
+ * https://leetcode.com/problems/keep-multiplying-found-values-by-two/
+ */
+class KeepMultiplyingByTwo : Problem {
+
+    fun findFinalValue(nums: IntArray, original: Int): Int {
+        do {
+            var swapped = false
+            for (i in 0 until nums.size - 1) {
+                if (nums[i] > nums[i + 1]) {
+                    val temp = nums[i]
+                    nums[i] = nums[i + 1]
+                    nums[i + 1] = temp
+                    swapped = true
+                }
+            }
+        } while (swapped)
+
+        var result = original
+        for (n in nums) {
+
+            if (result == n) {
+                result *= 2
+            }
+        }
+
+        return result
+    }
+
+    override fun run() {
+        println(findFinalValue(intArrayOf(5, 3, 6, 1, 12), 3))
+        println(findFinalValue(intArrayOf(2, 7, 9), 4))
+    }
+
+}
