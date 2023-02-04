2215d1180543a1e59c90784d4267b8db61017752
trenatov <temirlan.renatov@gmail.com>
2023-02-04 17:59:03 +0600

    changes

--- src/main/kotlin/string/PermutationInString.kt
+++ src/main/kotlin/string/PermutationInString.kt
@@ -9,11 +9,10 @@ import Problem
 class PermutationInString : Problem {
 
     fun checkInclusion(s1: String, s2: String): Boolean {
-        val restLength = s1.length - 1
         val sortedS1 = s1.toCharArray().sorted()
 
         for (i in 0..s2.length - s1.length) {
-            val substring = s2.substring(i, i + restLength + 1).toCharArray().sorted().toCharArray()
+            val substring = s2.substring(i, i + s1.length).toCharArray().sorted().toCharArray()
             if (sortedS1.toCharArray().contentEquals(substring)) {
                 return true
             }
